6881d4117cb9a4121f3a5ad64617e18afd8b6d1c
Frank Aveni <frank@phantomhand.com>
2017-01-15 23:30:35 -0500

    Code the Single post template
    Complete through Section 11, Lecture 64 – Code the Full Blog Post

--- b2w/wp-content/themes/bootstrap2wordpress/functions.php
+++ b2w/wp-content/themes/bootstrap2wordpress/functions.php
@@ -92,8 +92,8 @@ function bootstrap2wordpress_widgets_init() {
 		'description'   => esc_html__( 'Add widgets here.', 'bootstrap2wordpress' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
 	) );
 }
 add_action( 'widgets_init', 'bootstrap2wordpress_widgets_init' );
@@ -138,3 +138,12 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Replaces the excerpt "more" text by a link.
+ */
+function new_excerpt_more($more) {
+    global $post;
+	return '... <a class="moretag" href="'. get_permalink($post->ID) . '"> continue reading &raquo;</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
--- b2w/wp-content/themes/bootstrap2wordpress/single.php
+++ b2w/wp-content/themes/bootstrap2wordpress/single.php
@@ -2,34 +2,40 @@
 /**
  * The template for displaying all single posts.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package Bootstrap_to_Wordpress
+ * @package Bootstrap to WordPress
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<!-- BLOG CONTENT
+	================================================== -->
+	<div class="container">
+		<div class="row" id="primary">
+				
+			<main id="content" class="col-sm-8">
 
-		<?php
-		while ( have_posts() ) : the_post();
+			<?php while ( have_posts() ) : the_post(); ?>
+	
+				<?php get_template_part( 'template-parts/content', 'single' ); ?>
+	
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+	
+			<?php endwhile; // end of the loop. ?>
 
-			get_template_part( 'template-parts/content', get_post_format() );
+			</main><!-- #content -->
+			
+			<!-- SIDEBAR
+			================================================== -->
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+			
+		</div><!-- #primary -->
+	</div><!-- .container -->
 
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
