d5b7db680cd239d262e3f8417bac3a66e072c616
Frank Aveni <frank@phantomhand.com>
2017-01-16 23:11:12 -0500

    Code the Search template
    Completed Section 11, Lecture 66 – Customize the Search Results Template

--- b2w/wp-content/themes/bootstrap2wordpress/search.php
+++ b2w/wp-content/themes/bootstrap2wordpress/search.php
@@ -9,15 +9,16 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
 				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'bootstrap2wordpress' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+	</section>
+	
+	<div class="container">
+		<div id="primary" class="row">
+		
+			<main id="content" class="col-sm-8"> 
+
+		<?php if ( have_posts() ) : ?>
 
 			<?php
 			/* Start the Loop */
@@ -32,16 +33,22 @@ get_header(); ?>
 
 			endwhile;
 
-			the_posts_navigation();
-
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
+		
+		</main><!-- #content -->
+		
+		<!-- SIDEBAR
+		================================================== -->
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</aside>
+		
+	</div><!-- #primary -->
+</div><!-- .container -->
 
 <?php
 get_sidebar();
