1de7182b9e64e41f28634d44e77296756239cec3
Frank Aveni <frank@phantomhand.com>
2017-01-20 23:05:47 -0500

    Add extra (custom) sidebar to index page
    Complete through Section 13, Lecture 69

--- b2w/wp-content/themes/bootstrap2wordpress/index.php
+++ b2w/wp-content/themes/bootstrap2wordpress/index.php
@@ -14,43 +14,51 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+		<h1 class="page-title">Blog</h1>
+	</section>
+	
+	<!-- BLOG CONTENT
+	================================================== -->	
+	
+	<div class="container">
+		<div class="row" id="primary">
+			
+			<main id="content" class="col-sm-8" role="main">
+			
+			<?php if ( have_posts() ) : ?>
 
-		<?php
-		if ( have_posts() ) :
+			<?php /* Start the Loop */ ?>
+				
+				<?php while ( have_posts() ) : the_post(); ?>
+	
+					<?php 
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
+					?>
+	
+				<?php endwhile; ?>
+	
+			<?php else : ?>
+	
+				 <?php get_template_part( 'template-parts/content', 'none' ); ?>
+	
+			<?php endif; ?>
+						
+			</main> <!-- content -->
+			
+		<!-- SIDEBAR
+		================================================== -->
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+			<?php dynamic_sidebar( 'sidebar-2'); ?>
+		</aside>
+				
+		</div> <!-- #primary -->
+	</div> <!-- #container -->
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
